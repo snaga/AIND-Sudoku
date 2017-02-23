ae08a76dab754339eb8b27522636d89944aa935c
Satoshi Nagayasu <snaga@uptime.jp>
2017-02-23 21:54:32 +0900

    WIP: Project: Solve a Sudoku with AI
    Add diagonal units.
    Add solve().

--- solution.py
+++ solution.py
@@ -36,7 +36,9 @@ boxes = cross(rows, cols)
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-unitlist = row_units + column_units + square_units
+diagonal_units = [[x[0]+x[1] for x in zip('ABCDEFGHI', '123456789')], [x[0]+x[1] for x in zip('ABCDEFGHI', '987654321')]]
+
+unitlist = row_units + column_units + square_units + diagonal_units
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
@@ -182,6 +184,8 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
+    values = grid_values(grid)
+    return search(values)
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
